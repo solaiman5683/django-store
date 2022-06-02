6b4d17308aaa6b1bcb8ddae12dd7df3a14eff550
MD. Sulaiman Hosain <solaiman.sh6@gmail.com>
2022-06-02 19:28:07 +0600

    Modals updarted

--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product, OrderItem
 
 # Create your views here.
 
 
 def hello(request):
+    # query_set = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
+
     return render(request, 'hello.html', {
         'name': 'Sulaiman Hosain',
         'age': '24',
         'hobbies': ['Coding', 'Reading', 'Watching Movies'],
+        'products': list( query_set )
     })
